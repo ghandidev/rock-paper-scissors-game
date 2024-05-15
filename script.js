f265c7b43c71a03c51a367b778ef4b04539710e5
ghandidev <ghandiaboultaif@gmail.com>
2024-05-15 18:18:25 -0400

    added a function to return the round winner

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// selecciona un valor aleatorio de un array
 function getRandomComputerResult() {
   const options = ['Rock', 'Paper', 'Scissors'];
-  const random = options[Math.floor(Math.random() * options.length)];
-  return random;
+  const random = Math.floor(Math.random() * options.length);
+  return options[random];
 }
-
-console.log(getRandomComputerResult());
+let playerScore = 0;
+let computerScore = 0;
+// verifica quien a ganado la ronda
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === 'Rock' && computer === 'Scissors') ||
+    (player === 'Scissors' && computer === 'Paper') ||
+    (player === 'Paper' && computer === 'Rock')
+  );
+}
